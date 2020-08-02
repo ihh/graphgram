a54734b0f94285741994fb7c58d8d6423dbf514a
Ian Holmes <ihh@berkeley.edu>
2020-08-02 13:50:33 -0700

    eval

--- bin/emojis.js
+++ bin/emojis.js
@@ -55,7 +55,7 @@ if (!dotFile)
 
 const emojisFile = opt.options.emojis
 const emojis = (emojisFile
-                ? JSON.parse (eval ('(()=>{return ' + fs.readFileSync(emojisFile).toString() + '})()'))
+                ? eval ('(()=>{return ' + fs.readFileSync(emojisFile).toString() + '})()')
                 : defaultEmojis)
 
 let seed = opt.options.seed
