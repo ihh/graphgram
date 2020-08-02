115f3fe7a3893f949b4585625f303650226d2d5e
Ian Holmes <ihh@berkeley.edu>
2020-08-01 23:23:22 -0700

    emojis

--- bin/emojis.js
+++ bin/emojis.js
@@ -11,7 +11,17 @@ const fs = require('fs'),
 const defaultEmojis = {
   theme: {
     standard: {
-      
+      live: ['#happy'],
+      enter: ['thinking','wave','raised_hand'],
+      ignore: ['expressionless', 'running_man', 'running_woman'],
+      bypass: ['eyes', 'roll_eyes', 'unamused'],
+      scenery: ['#building','#plant','#flower','#nature'],
+      x: ['#nature'],
+      crossroads: ['#plus','#x','#road'],
+      fork: ['#y','#road'],
+      rumor: ['#secret'],
+      treasure: ['#money'],
+      door: ['door']
     }
   }
 };
@@ -19,7 +29,9 @@ const defaultEmojis = {
 const opt = getopt.create([
   ['d' , 'dot=PATH'    , 'GraphViz file (required)'],
   ['e' , 'emojis=PATH' , 'JSON emojis file (optional)'],
+  ['p' , 'print'       , 'print all emojis'],
   ['t' , 'theme=STRING', 'theme from emojis file (optional)'],
+  ['f' , 'fitzpatrick' , 'use Fitzpatrick modifiers'],
   ['s' , 'seed=N'      , 'seed random number generator'],
   ['h' , 'help'        , 'display this help message']
 ])              // create Getopt instance
@@ -45,14 +57,35 @@ const randElement = (array) => array[Math.floor (mt.rnd() * array.length)];
 
 const theme = emojis.theme[opt.options.theme || randElement (Object.keys(emojis.theme))]
 
-const toEmoji = (keyword) => {
-  let result = keyword
-  const emojiList = emojiSearch (keyword)
-  if (emojiList && emojiList.length) {
-    const emoji = randElement (emojiList)
+const toEmoji = (options) => {
+  if (opt.options.print) {
+    options.forEach ((keyword) => {
+      const emojiList = (keyword[0] === '#'
+                         ? emojiSearch (keyword.substr(1))
+                         : [extend ({name:keyword}, emojiLib.lib[keyword])])
+      console.warn (keyword + " => " + emojiList.map ((emoji) => {
+        if (!emoji)
+          throw new Error ('Emoji ' + keyword + ' not found')
+        return emoji.name + emoji.char
+      }).join(","))
+    })
+  }
+  const keyword = randElement (options)
+  let result = keyword, emoji
+  if (keyword[0] === '#') {
+    const emojiList = emojiSearch (keyword.substr(1))
+    if (emojiList && emojiList.length) {
+      emoji = randElement (emojiList)
+    }
+  } else
+    emoji = extend ({ name: keyword }, emojiLib.lib[keyword])
+  if (emoji) {
     result = emoji.char
-    if (emoji.fitzpatrick_scale)
-      result += randElement (emojiLib.fitzpatrick_scale_modifiers)
+    if (opt.options.fitzpatrick && emoji.fitzpatrick_scale) {
+      const mod = randElement (emojiLib.fitzpatrick_scale_modifiers)
+      const modResult = result + mod
+      result = modResult.length < 6 ? modResult : result  // hack to avoid double characters
+    }
   }
   return result
 }
@@ -60,8 +93,10 @@ const toEmoji = (keyword) => {
 fs.readFileSync(dotFile).toString()
   .split ("\n")
   .forEach ((line) => {
-    line = line.replace (/label="(.*)"/g, (_m, label) => {
-      return 'label="' + toEmoji (theme[label] || label) + '"'
+    line = line.replace (/label="(.*?)"/g, (_m, label) => {
+      const emoji = toEmoji (theme[label] || ['#' + label])
+      console.warn ("Replacing " + label + " with " + emoji)
+      return 'label="' + emoji + '"'
     })
     console.log (line)
   })
